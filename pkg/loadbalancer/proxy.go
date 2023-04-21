48c928ba42246cb34ef61f94c0ad7ac5edff6bc6
czybjtu <smartczy@outlook.com>
2023-04-21 10:27:53 +0800

    feat: support ipv6
    Signed-off-by: czybjtu <smartczy@outlook.com>

--- pkg/loadbalancer/proxy.go
+++ pkg/loadbalancer/proxy.go
@@ -26,8 +26,14 @@ const proxyConfigPath = "/usr/local/etc/haproxy/haproxy.cfg"
 type proxyConfigData struct {
 	ServicePorts    []string
 	HealthCheckPort int
-	BackendServers  map[string]string
+	BackendServers  map[string][]backendServer
 	IPv6            bool
+	IPv4            bool
+}
+
+type backendServer struct {
+	Name    string
+	Address string
 }
 
 // proxyDefaultConfigTemplate is the loadbalancer config template
@@ -51,16 +57,41 @@ defaults
   # allow to boot despite dns don't resolve backends
   default-server init-addr none
 
-frontend service
-{{ range $index, $port := .ServicePorts }}  bind *:{{ $port }}{{end}}
-  default_backend nodes
+{{- if .IPv4}}
+frontend ipv4-frontend
+{{- $bind := "*:" }}
+{{- range $index, $port := .ServicePorts }} 
+  bind {{$bind}}{{ $port }}  
+{{- end}} 
+  default_backend ipv4-backend
+{{end}}
 
-backend nodes
+{{- if .IPv6}}
+frontend ipv6-frontend
+{{- $bind := ":::"}}
+{{- range $index, $port := .ServicePorts }} 
+  bind {{$bind}}{{ $port }}  
+{{- end}} 
+  default_backend ipv6-backend
+{{end}}
+
+{{- if .IPv4}}
+backend ipv4-backend
   option httpchk GET /healthz
   {{- $hcport := .HealthCheckPort -}}
-  {{- range $server, $address := .BackendServers }}
-  server {{ $server }} {{ $address }} check port {{ $hcport }} inter 5s fall 3 rise 1
+  {{- range $i, $server := index .BackendServers "IPv4" }}
+  server {{ $server.Name }} {{ $server.Address }} check port {{ $hcport }} inter 5s fall 3 rise 1
   {{- end}}
+{{end}}
+
+{{- if .IPv6}}
+backend ipv6-backend
+  option httpchk GET /healthz
+  {{- $hcport := .HealthCheckPort -}}
+  {{- range $i, $server := index .BackendServers "IPv6" }}
+  server {{ $server.Name }} {{ $server.Address }} check port {{ $hcport }} inter 5s fall 3 rise 1
+  {{- end}}
+{{- end}}
 `
 
 // proxyConfig returns a kubeadm config generated from config data, in particular
@@ -86,37 +117,44 @@ func proxyUpdateLoadBalancer(ctx context.Context, clusterName string, service *v
 	}
 	config := &proxyConfigData{
 		HealthCheckPort: 10256, // kube-proxy default port
-		BackendServers:  map[string]string{},
+		BackendServers:  map[string][]backendServer{},
 		ServicePorts:    []string{},
 	}
+
+	config.IPv6 = len(service.Spec.IPFamilies) == 2 || service.Spec.IPFamilies[0] == v1.IPv6Protocol
+	config.IPv4 = len(service.Spec.IPFamilies) == 2 || service.Spec.IPFamilies[0] == v1.IPv4Protocol
 	if service.Spec.ExternalTrafficPolicy == v1.ServiceExternalTrafficPolicyTypeLocal {
 		config.HealthCheckPort = int(service.Spec.HealthCheckNodePort)
 	}
 
-	backendsV4 := map[string]string{}
-	backendsV6 := map[string]string{}
+	backends := map[string][]backendServer{}
 	for _, n := range nodes {
 		for _, addr := range n.Status.Addresses {
 			if addr.Type == v1.NodeInternalIP {
 				if netutils.IsIPv4String(addr.Address) {
-					backendsV4[n.Name] = addr.Address
+					backends[string(v1.IPv4Protocol)] = append(backends[string(v1.IPv4Protocol)], backendServer{Name: n.Name, Address: addr.Address})
 				} else if netutils.IsIPv6String(addr.Address) {
-					backendsV6[n.Name] = addr.Address
+					backends[string(v1.IPv6Protocol)] = append(backends[string(v1.IPv6Protocol)], backendServer{Name: n.Name, Address: addr.Address})
 				}
 			}
 		}
 	}
 
-	// TODO: support UDP and IPv6
+	// TODO: support UDP
 	for _, port := range service.Spec.Ports {
 		if port.Protocol != v1.ProtocolTCP {
 			continue
 		}
 		config.ServicePorts = append(config.ServicePorts, strconv.Itoa(int(port.Port)))
-		for server, address := range backendsV4 {
-			config.BackendServers[server] = net.JoinHostPort(address, strconv.Itoa(int(port.NodePort)))
+
+		for _, be := range backends {
+			for i := range be {
+				be[i].Address = net.JoinHostPort(be[i].Address, strconv.Itoa(int(port.NodePort)))
+			}
 		}
 	}
+	config.BackendServers = backends
+	klog.V(2).Infof("backend servers info: %v", backends)
 
 	// create loadbalancer config data
 	loadbalancerConfig, err := proxyConfig(config)
