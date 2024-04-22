bd69d5a8e8d3ed6968f63181a826f9f114b470fc
Gary Archer <garyarcher36@gmail.com>
2024-04-22 09:47:15 +0100

    Add the command to add an IP to an interface on Windows

--- pkg/loadbalancer/address_windows.go
+++ pkg/loadbalancer/address_windows.go
@@ -2,12 +2,22 @@
 
 package loadbalancer
 
-import "fmt"
+import (
+	"os/exec"
+)
 
 func AddIPToInterface(ifaceName string, ip string) error {
-	return fmt.Errorf(("not implemented"))
+	err := exec.Command("netsh", "interface", "ip", "add", "address", "loopback", ip, "255.255.255.255").Run()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func RemoveIPToInterface(ifaceName string, ip string) error {
-	return fmt.Errorf(("not implemented"))
+	err := exec.Command("netsh", "interface", "ip", "delete", "address", "loopback", ip, "255.255.255.255").Run()
+	if err != nil {
+		return err
+	}
+	return nil
 }
