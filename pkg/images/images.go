b37f0f0470ecf10deddabd08e876e8ac7eb16fda
rihib <rihib@rihib.dev>
2024-12-08 12:51:58 +0000

    fix comment

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -1,6 +1,5 @@
 package images
 
 var Images = map[string]string{
-	// ProxyImage defines the loadbalancer image:tag
-	"proxy": "docker.io/envoyproxy/envoy:v1.30.1",
+	"proxy": "docker.io/envoyproxy/envoy:v1.30.1", // proxy defines the loadbalancer image:tag
 }
