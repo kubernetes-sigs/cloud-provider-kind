ef719082771e7b82139fc27a597212ed4d553318
Dave Protasowski <dprotaso@gmail.com>
2025-01-29 19:04:05 -0500

    If the IP is on the lo device already don't fail

--- pkg/loadbalancer/address_other.go
+++ pkg/loadbalancer/address_other.go
@@ -2,11 +2,21 @@
 
 package loadbalancer
 
-import "os/exec"
+import (
+	"bytes"
+	"errors"
+	"os/exec"
+)
 
 func AddIPToLocalInterface(ip string) error {
-	err := exec.Command("ip", "addr", "add", ip, "dev", "lo").Run()
-	if err != nil {
+	outBytes, err := exec.Command("ip", "addr", "add", ip, "dev", "lo").CombinedOutput()
+
+	var exerr *exec.ExitError
+	if errors.As(err, &exerr) && bytes.ContainsAny(outBytes, "File exists") {
+		// unsure why but `Run()` doesn't capture stderr properly
+		// if 'File exists' is in the output, then the lo device has the IP already
+		return nil
+	} else if err != nil {
 		return err
 	}
 	return nil
